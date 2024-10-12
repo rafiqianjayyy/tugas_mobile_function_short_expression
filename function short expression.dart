004ebd03931c41aad7acd8e420d12ef273396b5f
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:36:24 +0700

    Create function short expression.dart

---
+++ function short expression.dart
@@ -0,0 +1,7 @@
+//INI ADALAH CONTOH PENGGUNAAN FUNCTION SHORT EXPRESSION
+// int Tambah(int awal, int akhir) => awal + akhir;
+
+// void main(){
+//   print(Tambah(10,12));
+//   print(Tambah(10,13));
+// }
